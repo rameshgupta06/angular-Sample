94e8f142f741d2e6501309b4f60cbef4471f3596
rameshgupta06 <rameshguptarg07@gmail.com>
2020-03-26 01:56:41 +0530

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,12 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
+  Share() {
     window.alert('The product has been shared!');
+  }
+
+  NotifyMe(){
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
